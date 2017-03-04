3f6c6a631df8293e856beddcec564ff4b8e47af6
Tiffany Thompson <Tiffany Thompson>
2017-03-03 20:14:41 -0800

    Added messages to user for Steps 1-5

--- EDAChecklist.R
+++ EDAChecklist.R
@@ -1,45 +1,60 @@
-library(readr)
+library(readr) # Add these packages to packrat?
 library(dplyr)
+
+cat("This script demonstrates the Exploratory Data Analysis Checklist, as described in
+Peng and Matsui's 'The Art of Data Science.'
+
+I will be working with hourly ozone data from 2016, published by the EPA.\n")
 
 ######################################
 # 1: Read in the Data
 ######################################
-cat("Reading in EPA Ozone data...")
+cat("Step 1: Read in the Data
+
+Fetching the data from the EPA website...\n")
 zipFile <- tempfile()
 download.file("http://aqsdr1.epa.gov/aqsweb/aqstmp/airdata/hourly_44201_2016.zip",zipFile)
+cat("Reading in the data...\n")
 ozone <- read_csv(unz(zipFile,"hourly_44201_2016.csv"))
 unlink(zipFile)
 names(ozone) <- make.names(names(ozone))
+cat("Data set read in successfully.\n")
 
 #######################################
 # 2: Check the Metadata
 #######################################
-cat("Check number rows, number columns, data fields, data types:")
+cat("Step 2: Check the Metadata (aka Check Your n's)\n")
 str(ozone)
+cat("Confirm that the number of rows, the number of columns, and the data fields + data types
+are what you expected.\n")
 
 #######################################
 # 3: Look at Top and Bottom of Data
 #######################################
-cat("Check top and bottom of data for formatting issues/garbage rows:")
+cat("Step 3: Look at the Top and Bottom of the Data Set\n")
 head(ozone)
 tail(ozone)
+cat("Check the data above for formatting issues or garbage rows.\n")
 
 #######################################
-# 4: Check "n"s against Landmarks
+# 4: Check n's against Landmarks
 #######################################
-cat("Check that all hours are represented:")
+cat("Step 4: Check 'n's against Landmarks\n")
 head(table(ozone$Time.Local))
-cat("Check that all states are represented:")
+cat("Check that all hours are represented above.")
 select(ozone,State.Name) %>% unique %>% nrow
-cat("Why are there 52 states? Inspect unique elements of State.Name:")
+cat("Check that all states are represented.\n
+Why are there 52 states? Inspect unique elements of State.Name:")
 unique(ozone$State.Name)
-cat("Puerto Rico and Washington DC are valid U.S. locations.")
+cat("Puerto Rico and Washington DC are valid U.S. locations.
+So far, this data is valid.\n")
 
 ######################################
 # 5: Validate with External Data
 ######################################
-cat("Compare data against EPA's 2015 acceptable max concentration: 0.070 ppm")
+cat("Step 5: Validate with External Data\n")
 quantile(ozone$Sample.Measurement, seq(0,1,0.1))
+cat("Compare data above against EPA's 2015 acceptable max average concentration: 0.070 ppm\n")
 cat("Look at outliers:")
 filter(ozone,Sample.Measurement < 0) %>% select(State.Code,County.Code,Date.Local,Time.Local,Sample.Measurement)
 filter(ozone,Sample.Measurement > 0.070) %>% select(State.Code,County.Code,Date.Local,Time.Local,Sample.Measurement)
